04fcce9f7ebcab789cfba0e45214bd292b0ede0e
Shahbaz51 <rajpoot51king@gmail.com>
2024-11-12 20:39:32 +0500

    Initial commit

---
+++ app/components/hello.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react'
+
+export default function hello() {
+    console.log("I am a Client component!")
+  return (
+    <div>
+       <h1>Hello</h1>
+    </div>
+  )
+}
